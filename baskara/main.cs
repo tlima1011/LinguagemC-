e1bfa2bf5ebe0f0f5f4d5e0020c6a1ea0e7515ff
Thiago <thiagolima1011@gmail.com>
2020-08-19 10:33:06 -0300

    Equação de Segundo grau

---
+++ baskara/main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+
+namespace baskara {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, delta = 0;
+            Console.Write("Coeficiente a: ");
+            a = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Coeficiente b: ");
+            b = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Coeficiente c: ");
+            c = double.Parse(Console.ReadLine(), CI);
+
+            delta = Math.Pow(b, 2) - (4 * a * c);
+            if (a == 0 || delta < 0) {
+                Console.WriteLine("Esta equacao nao possui raizes reais");
+            }
+            else {
+                x1 = -(-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = -(-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("X1 = " + x1.ToString("F4", CI));
+                Console.WriteLine("X2 = " + x2.ToString("F4", CI));
+            }
+        }
+    }
+}
