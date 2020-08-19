44c5afec23981a96a2c372771c72fe1691c7f247
Thiago <thiagolima1011@gmail.com>
2020-08-19 10:57:29 -0300

    Negativos Matriz

---
+++ negativos_matriz/main.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace negativos_matriz {
+    class Program {
+        static void Main(string[] args) {
+
+            int m = 0, n = 0;
+
+            Console.Write("Qual a quantidade de linhas da matriz? ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual a quantidade de colunas da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    Console.Write("Elemento["+ i + "," + j +"]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] < 0) {
+                        Console.WriteLine(mat[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
