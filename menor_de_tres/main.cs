7eacfc02c5d45343a844189927dbfe5aeebc6763
Thiago <thiagolima1011@gmail.com>
2020-08-19 10:55:11 -0300

    Menor de Tres

---
+++ menor_de_tres/main.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace menor_de_tres {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 0, b = 0, c = 0, menor = 0;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+            
+            if (a < b && a < c) {
+                menor = a;
+            }
+            else if (b < c) {
+                menor = b;
+            }
+            else {
+                menor = c;
+            }
+
+            Console.WriteLine("MENOR = " + menor);
+        }
+    }
+}
